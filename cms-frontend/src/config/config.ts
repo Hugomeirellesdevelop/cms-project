912356065d3ca7d27748dd9e60d8f94055fcb9b6
cota2web <cota2web@gmail.com>
2025-02-11 20:49:50 -0300

    Teste ambiente

--- cms-frontend/src/config/config.ts
+++ cms-frontend/src/config/config.ts
@@ -1,11 +1,11 @@
 export const configApp = {
     app: {
-        urlBase: process.env.NEXT_PUBLIC_API_URL || '82.29.60.183',
-        port: process.env.NEXT_PUBLIC_API_PORT || '3001'
+        urlBase: '82.29.60.183',
+        port:  '3000'
     },
     api: {
-        urlBase: process.env.NEXT_PUBLIC_API_URL || '82.29.60.183',
-        port: process.env.NEXT_PUBLIC_API_PORT || '3001'
+        urlBase:  '82.29.60.183',
+        port: '3001'
     },
     routes: {
         categorias: '/categorias',
