fac5102ebaad8575aef44669e993af8c727a1174
cota2web <cota2web@gmail.com>
2025-02-11 20:44:43 -0300

    teste verificação .env front

--- cms-frontend/src/config/config.ts
+++ cms-frontend/src/config/config.ts
@@ -1,11 +1,11 @@
 export const configApp = {
     app: {
-        urlBase: process.env.NEXT_PUBLIC_API_URL,
-        port: process.env.NEXT_PUBLIC_API_PORT
+        urlBase: process.env.NEXT_PUBLIC_API_URL || '82.29.60.183',
+        port: process.env.NEXT_PUBLIC_API_PORT || '3001'
     },
     api: {
-        urlBase: process.env.NEXT_PUBLIC_API_URL,
-        port: process.env.NEXT_PUBLIC_API_PORT
+        urlBase: process.env.NEXT_PUBLIC_API_URL || '82.29.60.183',
+        port: process.env.NEXT_PUBLIC_API_PORT || '3001'
     },
     routes: {
         categorias: '/categorias',
