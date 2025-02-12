aa5fe6c6234a6b9245344690472bc1b4d3c4ccc4
Hugomeirellesdevelop <hugomeirelles.develop@gmail.com>
2025-02-12 20:14:30 +0000

    image config corrigido

--- cms-frontend/next.config.ts
+++ cms-frontend/next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '82.29.60.183',
+        port: '3001',
+        pathname: '/posts/image/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;
